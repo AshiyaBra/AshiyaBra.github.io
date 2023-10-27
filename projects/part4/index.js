37082b7acae437c2c200fcb8b0eb3fc59713faa5
Ashiya <Ashiyabranch2@gmail.com>
2023-10-27 15:11:15 -0400

    test

--- projects/part4/index.js
+++ projects/part4/index.js
@@ -3,20 +3,20 @@ const toggleNav = () => {
 };
 
 const getInfo = async () => {
-    const url = "https://ashiyabra.github.io/json/index.json";
-    try{
+    const url = "https://ashiyabra.github.io/projects/part4/json/index.json";
+    try {
         const response = await fetch(url);
         return await response.json();
-    }catch(error){
+    } catch (error) {
         console.error('Error:', error);
     }
 };
 
 const displayInfo = async () => {
     const info = await getInfo();
-    const infoContainer = document.getElementById("main-sales"); 
+    const infoContainer = document.getElementById("main-content"); 
     info.forEach((info) => {
-        infoContainer.appendChild(getSectionInfo(info)); 
+        infoContainer.appendChild(getSectionInfo(info));
     });
 };
 
@@ -24,28 +24,21 @@ const getSectionInfo = (info) => {
     const section = document.createElement("section");
 
     const h2 = document.createElement("h2");
-    h2.innerHTML = info.name; 
+    h2.innerHTML = info.name;
 
-    const link = document.createElement("p");
-    link.innerHTML = `<strong>Genres: </strong> ${info.link}`;
 
     const description = document.createElement("p");
     description.innerHTML = `<strong>Description: </strong> ${info.description}`;
 
     const img = document.createElement("img");
-    img.src = `https://ashiyabra.github.io/json/${info.image}`;
-    
-
+    img.src = `https://ashiyabra.github.io/projects/part4/${info.image}`; 
 
     section.appendChild(h2);
-    section.appendChild(link);
     section.appendChild(description);
     section.appendChild(img);
 
-    return section; 
+    return section;
 };
-
-
 
 window.onload = () => {
     document.getElementById("hamburger").addEventListener("click", toggleNav);
--- projects/part4/script.js
+++ projects/part4/script.js
@@ -32,7 +32,85 @@ const previewBox = () =>{
         }
     };
 };
+
+const getInfo = async () => {
+    const url = "https://ashiyabra.github.io/projects/part4/json/tabletsphones.json";
+    try {
+        const response = await fetch(url);
+        jsonData = await response.json();
+        displayTablets();
+        displayPhones();
+        displayTrendingProducts();
+        displaySales();
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+const displayTablets = () => {
+    const tabletsData = jsonData.tablets; 
+    const tabletsContainer = document.getElementById("cat-tablets"); 
+    tabletsData.forEach((tablet) => {
+        const tabletSection = getSectionInfo(tablet);
+        tabletsContainer.appendChild(tabletSection);
+    });
+};
+
+
+const displayPhones = () => {
+    const phonesData = jsonData.phones; 
+    const phonesContainer = document.getElementById("cat-phones"); 
+    phonesData.forEach((phone) => {
+        const phoneSection = getSectionInfo(phone);
+        phonesContainer.appendChild(phoneSection);
+    });
+};
+
+const displayTrendingProducts = () => {
+    const productsData = jsonData.product; 
+    const productsContainer = document.getElementById("cat-trendingProducts"); 
+    productsData.forEach((product) => {
+        const productSection = getSectionInfo(product);
+        productsContainer.appendChild(productSection);
+    });
+};
+
+
+const displaySales = () => {
+    const salesData = jsonData.sale; 
+    const salesContainer = document.getElementById("cat-sales"); 
+    phonesData.forEach((sale) => {
+        const saleSection = getSectionInfo(sale);
+        salesContainer.appendChild(saleSection);
+    });
+};
+
+const getSectionInfo = (info) => {
+    const section = document.createElement("section");
+
+    const newPrice = document.createElement("p");
+    newPrice.innerHTML = `<strong>New Price: </strong> ${info.newPrice}`;
+
+    const newOld = document.createElement("p");
+    newOld.innerHTML = `<strong>New Old: </strong> ${info.newOld}`;
+
+    const facts = document.createElement("p");
+    facts.innerHTML = `<strong>Description: </strong> ${info.facts}`;
+
+    const img = document.createElement("img");
+    img.src = `https://ashiyabra.github.io/projects/part4/${info.image}`;
+
+    section.appendChild(newPrice);
+    section.appendChild(newOld);
+    section.appendChild(facts);
+    section.appendChild(img);
+
+    return section;
+};
+
 window.onload = () => {
     document.getElementById("hamburger").addEventListener("click", toggleNav);
     previewBox();
+    getInfo();
+    
 };
