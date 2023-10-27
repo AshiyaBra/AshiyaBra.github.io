ac7000458e6ad950a38bc1ed3f117019ac0e9b62
Ashiya <Ashiyabranch2@gmail.com>
2023-10-27 11:28:58 -0400

    test

---
+++ projects/part4/index.js
@@ -0,0 +1,53 @@
+const toggleNav = () => {
+    document.getElementById("nav-pages").classList.toggle("hide-small");
+};
+
+const getInfo = async () => {
+    const url = "https://ashiyabra.github.io/json/index.json";
+    try{
+        const response = await fetch(url);
+        return await response.json();
+    }catch(error){
+        console.error('Error:', error);
+    }
+};
+
+const displayInfo = async () => {
+    const info = await getInfo();
+    const infoContainer = document.getElementById("main-sales"); 
+    info.forEach((info) => {
+        infoContainer.appendChild(getSectionInfo(info)); 
+    });
+};
+
+const getSectionInfo = (info) => {
+    const section = document.createElement("section");
+
+    const h2 = document.createElement("h2");
+    h2.innerHTML = info.name; 
+
+    const link = document.createElement("p");
+    link.innerHTML = `<strong>Genres: </strong> ${info.link}`;
+
+    const description = document.createElement("p");
+    description.innerHTML = `<strong>Description: </strong> ${info.description}`;
+
+    const img = document.createElement("img");
+    img.src = `https://ashiyabra.github.io/json/${info.image}`;
+    
+
+
+    section.appendChild(h2);
+    section.appendChild(link);
+    section.appendChild(description);
+    section.appendChild(img);
+
+    return section; 
+};
+
+
+
+window.onload = () => {
+    document.getElementById("hamburger").addEventListener("click", toggleNav);
+    displayInfo();
+};
