11f7f34b392435aaef802fa53eb6ff087705c854
Ashiya <Ashiyabranch2@gmail.com>
2023-10-29 10:29:02 -0400

    test

--- projects/part4/cameraaudio.js
+++ projects/part4/cameraaudio.js
@@ -2,39 +2,8 @@ const toggleNav = () => {
     document.getElementById("nav-pages").classList.toggle("hide-small");
 };
 
-const previewBox = () =>{
-    const back = document.getElementById("preview-back");
-    const content = document.querySelector(".content");
-
-    document.querySelectorAll(".items img").forEach((img) => {
-        img.onclick = () => {
-            const largeSrc = img.getAttribute("data-large-src");
-            document.querySelector(".content img").src = largeSrc;
-            back.style.display = "block";
-            content.classList.add("slideShow");
-
-            content.addEventListener("animationend", () => {
-                content.classList.remove("slideShow");
-                content.style.opacity = 1;
-            });
-        };
-    });
-
-    document.querySelector(".close").onclick = () => {
-        back.style.display = "none";
-        content.style.opacity = 0;
-    };
-
-    window.onclick = (event) => {
-        if (event.target == back) {
-            back.style.display = "none";
-            content.style.opacity = 0;
-        }
-    };
-};
-
 const getInfo = async () => {
-    const url = "https://ashiyabra.github.io/projects/part4/json/tabletsphones.json";
+    const url = "https://ashiyabra.github.io/projects/part4/json/cameraaudio.json";
     try {
         const response = await fetch(url);
         return await response.json();
@@ -143,6 +112,5 @@ const getSaleInfo = (sale) => {
 
 window.onload = () => {
     document.getElementById("hamburger").addEventListener("click", toggleNav);
-    previewBox();
     displayInfo();
 };
