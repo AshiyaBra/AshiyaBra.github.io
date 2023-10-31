b9be06c2dfaded48c026471e66909f52ed7e0f38
Ashiya <Ashiyabranch2@gmail.com>
2023-10-30 22:19:55 -0400

    test

--- projects/part4/sales.js
+++ projects/part4/sales.js
@@ -3,7 +3,7 @@ const toggleNav = () => {
 };
 
 const getInfo = async () => {
-    const url = "https://ashiyabra.github.io/projects/part4/json/gamingsystems.json";
+    const url = "https://ashiyabra.github.io/projects/part4/json/sales.json";
     try {
         const response = await fetch(url);
         return await response.json();
@@ -14,17 +14,28 @@ const getInfo = async () => {
 
 const displayInfo = async () => {
     const info = await getInfo();
-    const infoContainer = document.getElementById("products"); 
+    const infoContainer = document.getElementById("cat-content"); 
+
+    const categoryContainer = document.getElementById(category);
+    info.sales[category].forEach((product) => {
+        categoryContainer.appendChild(getProductInfo(product));
+    });
+
+    const trendingProductsContainer = document.getElementById("trending-products");
+    info.trendingProducts.forEach((trendingProduct) => {
+        trendingProductsContainer.appendChild(getTrendingProductInfo(trendingProduct));
+    });
 
     
-    info["products"].forEach((product) => {
-        infoContainer.appendChild(getProductInfo(product));
+    const salesContainer = document.getElementById("sales");
+    info.sales.forEach((sale) => {
+        salesContainer.appendChild(getSaleInfo(sale));
     });
 };
 
-const getProductInfo = (product) => {
+const getTabletsInfo = (product) => {
     const section = document.createElement("section");
-    
+
     const price = document.createElement("p");
     price.innerHTML = `<strong>Price: </strong> $${product.price}`;
 
@@ -32,8 +43,9 @@ const getProductInfo = (product) => {
     description.innerHTML = `<strong>Description: </strong> ${product.description}`;
 
     const img = document.createElement("img");
-    img.src = product.images;
+    img.src = product.image;
 
+   
     section.appendChild(price);
     section.appendChild(description);
     section.appendChild(img);
@@ -41,6 +53,111 @@ const getProductInfo = (product) => {
     return section;
 };
 
+const getPhonesInfo = (product) => {
+    const section = document.createElement("section");
+
+    const price = document.createElement("p");
+    price.innerHTML = `<strong>Price: </strong> $${product.price}`;
+
+    const description = document.createElement("p");
+    description.innerHTML = `<strong>Description: </strong> ${product.description}`;
+
+    const img = document.createElement("img");
+    img.src = product.image;
+
+   
+    section.appendChild(price);
+    section.appendChild(description);
+    section.appendChild(img);
+
+    return section;
+};
+
+const getCameraInfo = (product) => {
+    const section = document.createElement("section");
+
+    const price = document.createElement("p");
+    price.innerHTML = `<strong>Price: </strong> $${product.price}`;
+
+    const description = document.createElement("p");
+    description.innerHTML = `<strong>Description: </strong> ${product.description}`;
+
+    const img = document.createElement("img");
+    img.src = product.image;
+
+   
+    section.appendChild(price);
+    section.appendChild(description);
+    section.appendChild(img);
+
+    return section;
+};
+
+const getAudioInfo = (product) => {
+    const section = document.createElement("section");
+
+    const price = document.createElement("p");
+    price.innerHTML = `<strong>Price: </strong> $${product.price}`;
+
+    const description = document.createElement("p");
+    description.innerHTML = `<strong>Description: </strong> ${product.description}`;
+
+    const img = document.createElement("img");
+    img.src = product.image;
+
+   
+    section.appendChild(price);
+    section.appendChild(description);
+    section.appendChild(img);
+
+    return section;
+};
+
+const getGamingSystemsInfo = (sale) => {
+    const section = document.createElement("section");
+
+    const name = document.createElement("h2");
+    name.innerHTML = `<strong>Name: </strong> ${sale.name}`;
+    
+    const price = document.createElement("p");
+    price.innerHTML = `<strong>Price: </strong> $${sale.price}`;
+
+    const description = document.createElement("p");
+    description.innerHTML = `<strong>Description: </strong> ${sale.description}`;
+
+    const img = document.createElement("img");
+    img.src = sale.image;
+
+    section.appendChild(name);
+    section.appendChild(price);
+    section.appendChild(description);
+    section.appendChild(img);
+    
+    return section;
+};
+
+const getComputersInfo = (sale) => {
+    const section = document.createElement("section");
+
+    const name = document.createElement("h2");
+    name.innerHTML = `<strong>Name: </strong> ${sale.name}`;
+    
+    const price = document.createElement("p");
+    price.innerHTML = `<strong>Price: </strong> $${sale.price}`;
+
+    const description = document.createElement("p");
+    description.innerHTML = `<strong>Description: </strong> ${sale.description}`;
+
+    const img = document.createElement("img");
+    img.src = sale.image;
+
+    section.appendChild(name);
+    section.appendChild(price);
+    section.appendChild(description);
+    section.appendChild(img);
+    
+    return section;
+};
 
 window.onload = () => {
     document.getElementById("hamburger").addEventListener("click", toggleNav);
